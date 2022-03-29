15cd2803bec11d6f371783a8dafcf8ff18fb6edf
Nathan Kim <nkim3215@stu.d214.org>
2022-03-29 12:21:41 -0500

    Initial Commit

---
+++ BurgerHunter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerHunter
+//
+//  Created by Nathan Kim on 3/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
